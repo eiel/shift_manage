b98934e04889c5628f253249a14f3e48031dd671
morishita <tomohiro.m0219@gmail.com>
2013-04-13 13:04:16 +0900

    manage/shiftのcucumber作成開始

---
+++ features/step_definitions/shifts_steps.rb
@@ -0,0 +1,15 @@
+もし(/^'\/manage\/shifts\/new'にアクセス$/) do
+  visit("/manage/shifts/new")
+end
+
+もし(/^入力項目を入力する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^登録する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^シフトが作成される$/) do
+  pending # express the regexp above with the code you wish you had
+end
