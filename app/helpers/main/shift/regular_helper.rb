3e75a57b09fc8227d6a0af5e38c00534edc72a34
morishita <tomohiro.m0219@gmail.com>
2013-05-05 22:24:49 +0900

    登録項目に応じて勤務時間帯を表示する機能を実装

--- app/helpers/main/shift/regular_helper.rb
+++ app/helpers/main/shift/regular_helper.rb
@@ -12,4 +12,34 @@ module Main::Shift::RegularHelper
   	  	"勤務不可能"
   	end
   end 
+
+  def morning_work_time(status1, status2)
+  	if status1 == 2 && status2 == 2
+  	  "10:00 ~ 12:30"
+  	elsif status1 == 2 && status2 != 2
+  	  "10:00 ~ 10:30"
+  	elsif status1 != 2 && status2 == 2
+  	  "10:45 ~ 12:30"
+  	else
+  	  "-"
+  	end 
+  end
+
+  def afternoon_work_time(status1, status2, status3)
+  	if status1 == 2 && status2 == 2 && status3 == 2
+  	  "13:15 ~ 19:00"
+  	elsif status1 == 2 && status2 == 2 && status3 != 2
+  	  "13:15 ~ 16:30"
+  	elsif status1 == 2 && status2 != 2 && status3 != 2
+  	  "13:15 ~ 14:45"
+  	elsif status1 == 2 && status2 != 2 && status3 == 2
+  	  "13:15 ~ 14:45¥n16:45 ~ 19:00"
+  	elsif status1 != 2 && status2 == 2 && status3 != 2
+  	  "15:00 ~ 16:30"
+  	elsif status1 != 2 && status2 != 2 && status3 == 2
+  	  "16:45 ~ 19:00"
+  	else
+  	  "-"
+  	end 
+  end
 end
